82a7c07189efc16f63e6124adb800bf53a929ea7
codemesid <siddhanthbajoria@gmail.com>
2020-09-07 23:17:12 +0530

    tournament list made

---
+++ src/components/card/tournament.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+import { TournamentDoc } from "../../../server/models/tournament";
+import Router from 'next/router';
+import Link from 'next/link';
+
+const TournamentCard = ({ match }: { match: TournamentDoc }) => {
+    const [timer, setTimer] = useState('');
+    let stopTimer = false;
+
+    const getBalanceTimer = () => {
+        const sdt = new Date(match.startDateTime).getTime();
+        const cdt = new Date().getTime();
+        const msIn1Sec = 1000
+        const msIn1Min = msIn1Sec * 60;
+        const msIn1Hour = msIn1Min * 60;
+        const msIn1Day = msIn1Hour * 24;
+        const delta = sdt - cdt > 0 ? sdt - cdt : 0;
+        const daysLeft = Math.floor(delta / msIn1Day);
+        const hoursLeft = Math.floor((delta % (msIn1Day)) / (msIn1Hour));
+        const minsLeft = Math.floor((delta % (msIn1Hour)) / (msIn1Min));
+        const secondsLeft = Math.floor((delta % (msIn1Min)) / msIn1Sec);
+        const diffTime = `${daysLeft} Days ${hoursLeft < 10 ? `0${hoursLeft}` : hoursLeft}h ${minsLeft < 10 ? `0${minsLeft}` : minsLeft}m ${secondsLeft < 10 ? `0${secondsLeft}` : secondsLeft}s`
+
+
+        if (daysLeft >= 0 && hoursLeft >= 0 && minsLeft >= 0 && secondsLeft >= 0) {
+            if (daysLeft === 0 && hoursLeft === 0 && minsLeft === 0 && secondsLeft === 0) {
+                if (match.status === "upcoming") Router.reload();
+                stopTimer = true;
+            }
+            setTimer(diffTime);
+        }
+    }
+
+    useEffect(() => {
+        if (!stopTimer) setTimeout(getBalanceTimer, 1000);
+    }, [timer]);
+
+    return <Link href={`/tournaments/${match.id}`}>
+        <a className="match__footer__list__item">
+            <img src={match.game.imageUrl} alt={match.game.name} className="match__footer__list__item__image" />
+            <div className="match__footer__list__item__time">
+                {timer}
+            </div>
+        </a>
+    </Link>
+}
+
+export default TournamentCard;
--- src/pages/tournaments/index.tsx
+++ src/pages/tournaments/index.tsx
@@ -1,1 +1,98 @@
-export default () => <></>
+import { useState, useEffect } from 'react'
+import { serverRequest } from "../../hooks/server-request"
+import { TournamentDoc } from "../../../server/models/tournament";
+import MainLayout from "../../components/layout/mainlayout";
+import Tooltip from "../../components/tooltip";
+import Button from '../../components/button/main';
+import { AiOutlineMenu } from 'react-icons/ai';
+import TournamentCard from '../../components/card/tournament';
+
+const Tournaments = ({ tournaments }) => {
+    const [game, setGame] = useState('all');
+    const [type, setType] = useState("upcoming");
+    const [matches, setMatches] = useState([]);
+    useEffect(() => {
+        let newMatches = [];
+        if (game === "all") {
+            Object.keys(tournaments).forEach((key) => {
+                const m = tournaments[key].items[type];
+                if (m) newMatches.push(...m);
+            });
+        } else {
+            if (tournaments[game]) {
+                const m = tournaments[game]?.items[type];
+                if (m) newMatches = m;
+            }
+        }
+        setMatches(newMatches.filter(t => t));
+    }, [game, type])
+    return <MainLayout isFullscreen>
+        <div className="match">
+            <div className="match__head">
+                <div className="match__head__container">
+                    <div className="match__head__container__title">
+                        our tournaments
+                    </div>
+                    <div className="match__head__container__subtitle">
+                        This is where you can browse through our tournaments
+                    </div>
+                    <div className="match__head__container__list">
+                        <div className="match__head__container__list__item">
+                            <div className={`match__head__container__list__item__image ${game === 'all' ? "active" : ""} button`}>
+                                <Button onPress={() => setGame('all')} text={<AiOutlineMenu />} />
+                            </div>
+                        </div>
+                        {
+                            Object.keys(tournaments).map((key) => {
+                                const imageUrl = tournaments[key].game.thumbnailUrl;
+                                const gameName = tournaments[key].game.name
+                                return <Tooltip key={key} title={gameName}>
+                                    <div onClick={() => setGame(gameName)} className="match__head__container__list__item">
+                                        <img src={imageUrl} alt={gameName} className={`match__head__container__list__item__image ${game === gameName ? "active" : ""}`} />
+                                    </div>
+                                </Tooltip>
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+            <div className="match__footer">
+                <div className="match__footer__title">
+                    <div className="match__footer__title__main">Tournaments</div>
+                    <ul className="match__footer__title__side">
+                        {["upcoming", "started", "completed"].map(t => {
+                            return <li onClick={() => setType(t)} className={`match__footer__title__side__item ${type === t ? "active" : ""}`} key={t}>{t}</li>
+                        })}
+                    </ul>
+                </div>
+                <div className="match__footer__list">
+                    {matches.length >= 1 ? matches.map((match) => {
+                        return <TournamentCard key={match.name} match={match} />
+                    }) : <div style={{ textAlign: "center", fontSize: "2.5rem", color: "white", textTransform: "capitalize" }}>Sorry no tournaments available</div>}
+                </div>
+            </div>
+        </div>
+    </MainLayout>
+}
+
+Tournaments.getInitialProps = async (ctx) => {
+    const { data }: { data: Array<TournamentDoc> } = await serverRequest(ctx, { url: "/api/ugh/tournament/fetch/all", body: {}, method: "get" });
+    const tournaments = {}
+
+    data?.map(t => {
+        const gameName = t.game.name;
+        const status = t.status;
+        if (tournaments[gameName]) {
+            const statusKey = tournaments[gameName].items[status];
+            if (statusKey) tournaments[gameName].items[status].push(t)
+            else tournaments[gameName].items[status] = [t]
+        }
+        else tournaments[gameName] = {
+            items: { [status]: [t] },
+            game: t.game
+        }
+    });
+    return { tournaments }
+}
+
+export default Tournaments
