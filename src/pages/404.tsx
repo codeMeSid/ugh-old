a7214942b98b54e30a6159915378928f37186fb2
codemesid <siddhanthbajoria@gmail.com>
2020-10-06 13:49:35 +0530

    404 page style added

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,1 +1,9 @@
-export default () => <div>404</div>
+import MainLayout from "../components/layout/mainlayout"
+
+const IMG404 = require("../public/asset/404.gif")
+
+export default () => <MainLayout>
+    <div style={{ backgroundImage: `url(${IMG404})` }} className="wallpaper--404">
+
+    </div>
+</MainLayout>
