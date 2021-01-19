0ea2e1e104d899b31ed5e632c3b685d60d0b6611
codeMeSid <siddhanthbajoria@gmail.com>
2021-01-19 20:01:57 +0530

    tournament history logic update

--- server/controllers/tournament/fetch-ughId.ts
+++ server/controllers/tournament/fetch-ughId.ts
@@ -1,27 +1,36 @@
 import { Request, Response } from "express";
 import { Tournament } from "../../models/tournament";
 import { User } from "../../models/user";
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+import { BadRequestError } from "@monsid/ugh-og";
 
-export const tournamentFetchUghIdController = async (req: Request, res: Response) => {
-    const { ughId } = req.params
-    const user = await User.findOne({ ughId })
-    const tournamentsCount = user.tournaments.filter(t => !!!t.name);
-    if (tournamentsCount.length !== 0) {
-        const tournaments = await Tournament
-            .find({ _id: { $in: tournamentsCount.map(t => mongoose.Types.ObjectId(t.id)) } })
-            .select("name startDateTime game")
-            .populate("game", "name", "Games");
-        user.tournaments = user.tournaments.map(t => {
-            const tIndex = tournaments.findIndex(p => JSON.stringify(p.id) === JSON.stringify(t.id));
-            if (tIndex >= 0) {
-                t.name = tournaments[tIndex].name;
-                t.startDateTime = tournaments[tIndex].startDateTime;
-                t.game = tournaments[tIndex].game?.name;
-                t.endDateTime = tournaments[tIndex].endDateTime;
-            }
-            return t;
-        })
-    }
-    res.send(Array.from(user.toObject().tournaments).reverse());
-}
+export const tournamentFetchUghIdController = async (
+  req: Request,
+  res: Response
+) => {
+  const { ughId } = req.params;
+  const user = await User.findOne({ ughId });
+  if (!user) throw new BadRequestError("Invalid User Request");
+  //   const tournamentsCount = user.tournaments.filter((t) => !t.name);
+  //   if (tournamentsCount.length !== 0) {
+  //     const tournaments = await Tournament.find({
+  //       _id: { $in: tournamentsCount.map((t) => mongoose.Types.ObjectId(t.id)) },
+  //     })
+  //       .select("name startDateTime game")
+  //       .populate("game", "name", "Games");
+  //     user.tournaments = user.tournaments.map((t) => {
+  //       const tIndex = tournaments.findIndex(
+  //         (p) => JSON.stringify(p.id) === JSON.stringify(t.id)
+  //       );
+  //       if (tIndex >= 0) {
+  //         t.name = tournaments[tIndex].name;
+  //         t.startDateTime = tournaments[tIndex].startDateTime;
+  //         t.game = tournaments[tIndex].game?.name;
+  //         t.endDateTime = tournaments[tIndex].endDateTime;
+  //       }
+  //       return t;
+  //     });
+  //   }
+  res.send(user.tournaments.reverse());
+  //   res.send(Array.from(user.toObject().tournaments).reverse());
+};
